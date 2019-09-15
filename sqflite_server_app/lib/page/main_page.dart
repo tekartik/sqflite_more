90a703ed2bf1efd742dc2fb456d813f4da62af7f
alex <alex@tekartik.com>
2019-09-15 17:01:31 +0200

    lint: fix const warning

--- sqflite_server_app/lib/page/main_page.dart
+++ sqflite_server_app/lib/page/main_page.dart
@@ -179,7 +179,7 @@ class _SqfliteServerHomePageState extends State<SqfliteServerHomePage> {
   Future startApp() async {
     if (!app.started) {
       app.started = true;
-      await Future<dynamic>.delayed(Duration());
+      await Future<dynamic>.delayed(const Duration());
       //devPrint('startApp');
       portInputController.text = (app.prefs.port ?? 0).toString();
       if (app.prefs.autoStart) {
