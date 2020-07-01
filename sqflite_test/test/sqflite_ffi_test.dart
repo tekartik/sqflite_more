6d5e85799f5b2dd4461f1511f2fd97086794aea7
Alexandre Roux <alex@tekartik.com>
2020-07-01 20:06:42 +0200

    test: allow linux on dev

--- sqflite_test/test/sqflite_ffi_test.dart
+++ sqflite_test/test/sqflite_ffi_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:sqflite/sqflite_dev.dart';
 import 'package:sqflite_ffi_test/sqflite_ffi.dart';
 import 'package:sqflite_test/sqflite_test.dart';
 
@@ -13,7 +12,7 @@ void main() {
   // Set sqflite ffi support in test
   //TestWidgetsFlutterBinding.ensureInitialized();
   sqfliteFfiInit();
-  setMockDatabaseFactory(databaseFactoryFfi);
+  databaseFactory = databaseFactoryFfi;
 
   test('simplest', () async {
     var db = await openDatabase(inMemoryDatabasePath);
