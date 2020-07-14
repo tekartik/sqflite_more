36822c77ef6f0fd2c4eb8b4b5a22be074aaa62e3
alex <alex@tekartik.com>
2020-07-14 23:15:14 +0200

    feat: move from moor_ffi to sqlite3

--- sqflite_ffi_test/test/sqlite3_raw_ffi_test.dart
+++ sqflite_ffi_test/test/sqlite3_raw_ffi_test.dart
@@ -1,18 +1,18 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:moor_ffi/database.dart';
+import 'package:sqlite3/sqlite3.dart';
 import 'package:sqflite_ffi_test/src/windows/setup.dart';
 
 void main() {
-  test('moor_ffi simple test', () {
+  test('sqlite3 simple test', () {
     print(Directory.current);
     if (Platform.isWindows) {
       windowsInit();
     }
-    final database = Database.memory();
-    var version = database.userVersion();
+    final database = sqlite3.openInMemory();
+    var version = database.userVersion;
     expect(version, 0);
-    database.close();
+    database.dispose();
   });
 }
