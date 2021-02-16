5cd2d823bd4fb37d4991223e16fea296f7385eca
alex <alex@tekartik.com>
2021-02-16 23:23:26 +0100

    build: add github actions

---
+++ tool/run_ci.dart
@@ -0,0 +1,17 @@
+import 'package:dev_test/package.dart';
+
+Future main() async {
+  for (var dir in [
+    'sqflite_common_server',
+    'sqflite_common_test_app',
+    'sqflite_porter',
+    'sqflite_server',
+    'sqflite_test',
+    'sqflite_ffi_test',
+    'sqflite_test_app',
+    'sqflite_server_app',
+    'alt/sqflite_github_test'
+  ]) {
+    await packageRunCi(dir);
+  }
+}
